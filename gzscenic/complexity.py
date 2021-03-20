597d143453299a5dfddfadebf37d048273b16ced
Afsoon Afzal <afsoona@cs.cmu.edu>
2021-03-19 22:50:05 -0400

    Changes to complexity

--- gzscenic/complexity.py
+++ gzscenic/complexity.py
@@ -6,7 +6,7 @@ import yaml
 import numpy as np
 import matplotlib.pyplot as plt
 
-from shapely.geometry import Polygon, LineString
+from shapely.geometry import Polygon, LineString, Point
 from scenic.core.scenarios import Scene
 from scenic.core.object_types import Object
 from scenic.core.vectors import Vector
@@ -47,10 +47,18 @@ class Geometry:
 
 class Region:
 
-    def __init__(self, shape: Union[Polygon, LineString], included_geometries: List[Geometry]):
+    def __init__(self,
+                 waypoints: Tuple[Point, Point],
+                 shape: Union[Polygon, LineString],
+                 included_geometries: List[Geometry],
+                 vision_area: Polygon,
+                 in_vision_objects: List[Geometry]):
+        self.wps = waypoints
         self.shape = shape
         self.included_geometries = included_geometries
         self._is_polygon = isinstance(shape, Polygon)
+        self.vision_area = vision_area
+        self.in_vision_objects = in_vision_objects
 
     @property
     def coords(self):
@@ -60,12 +68,18 @@ class Region:
             return list(self.shape.coords)
 
     def to_dict(self):
-        d = {'area': self.shape.area,
+        d = {'wps': [tuple(p.coords)[0] for p in self.wps],
+             'area': self.shape.area,
              'length': self.shape.length,
              'centroid': list(self.shape.centroid.coords)[0],
              'minimum_clearance': self.shape.minimum_clearance,
              'coords': self.coords,
-             'included_geometries': [g.to_dict() for g in self.included_geometries]
+             'vision_area': {
+                 'area': self.vision_area.area,
+                 'coords': list(self.vision_area.exterior.coords)
+                 },
+             'included_geometries': [g.to_dict() for g in self.included_geometries],
+             'in_vision_objects': [g.to_dict() for g in self.in_vision_objects]
              }
         if self._is_polygon:
             mrr = self.shape.minimum_rotated_rectangle
@@ -82,7 +96,13 @@ class Region:
             shape = LineString(coords)
         else:
             shape = Polygon(coords)
-        return Region(shape, [Geometry.from_dict(g) for g in d['included_geometries']])
+        vision_area = Polygon(d['vision_area']['coords'])
+        wps = [Point(c) for c in d['wps']]
+        return Region(wps,
+                      shape,
+                      [Geometry.from_dict(g) for g in d['included_geometries']],
+                      vision_area,
+                      [Geometry.from_dict(g) for g in d['in_vision_objects']])
 
 
 def reset_objects(objects: List[Geometry]) -> None:
@@ -106,27 +126,39 @@ def plot(region: Region,
     ch_points = np.array(region.coords)
 
     all_points = np.array(all_points)
-    logger.info("all_points %s %s", all_points, all_points.shape)
-    logger.info("ch_points %s %s", ch_points, ch_points.shape)
+    vision_points = np.array(list(region.vision_area.exterior.coords))
+    #logger.info("all_points %s %s", all_points, all_points.shape)
+    #logger.info("ch_points %s %s", ch_points, ch_points.shape)
     plt.plot(all_points[:,0], all_points[:,1], 'o')
     plt.plot(ch_points[:,0], ch_points[:,1], 'k-')
+    plt.plot(vision_points[:,0], vision_points[:,1], 'y--')
 
-
-def scene_regions(scene: Scene) -> List[Region]:
+def scene_regions(scene: Scene) -> Tuple[List[Region], Tuple[float, float]]:
     
     ego = scene.egoObject
 
     waypoints = []
     objects = []
+    walls = []
+    wall_lengths = []
     for obj in scene.objects:
         if obj.type == ModelTypes.MISSION_ONLY:
             if obj == ego:
                 continue
             waypoints.append(obj)
         elif obj.gz_name == 'grey_wall' and obj.room_wall:
-            continue
+            hw, hl = obj.hw, obj.hl
+            wall_lengths.append(hl*2)
+            coords = [
+                    obj.relativePosition(Vector(hw, hl)).coordinates,
+                    obj.relativePosition(Vector(-hw, hl)).coordinates,
+                    obj.relativePosition(Vector(-hw, -hl)).coordinates,
+                    obj.relativePosition(Vector(hw, -hl)).coordinates
+                    ]
+            walls.append(Geometry(coords, obj.complexity, obj.gz_name))
         else:
-            hw, hl = obj.hw + ego.hw, obj.hl + ego.hl
+            #hw, hl = obj.hw + ego.hw, obj.hl + ego.hl
+            hw, hl = obj.hw + 0.05, obj.hl + 0.05
             coords = [
                     obj.relativePosition(Vector(hw, hl)).coordinates,
                     obj.relativePosition(Vector(-hw, hl)).coordinates,
@@ -140,6 +172,7 @@ def scene_regions(scene: Scene) -> List[Region]:
     for end in waypoints:
         reset_objects(objects)
         all_points = [start.position.coordinates, end.position.coordinates]
+        wps = (Point(start.position.coordinates), Point(end.position.coordinates))
         current_poly = LineString(all_points)
 
         while True:
@@ -156,19 +189,39 @@ def scene_regions(scene: Scene) -> List[Region]:
             new_poly = Polygon(all_points)
             current_poly = new_poly.convex_hull
         
-        region = Region(current_poly, [o for o in objects if o.visited])
+        vision_area = current_poly.buffer(2) # 2 meters around the region
+        in_vision_objects = [o for o in objects if not o.visited and vision_area.intersects(o.polygon)]
+        in_vision_objects.extend([w for w in walls if vision_area.intersects(w.polygon)])
+
+        region = Region(wps,
+                        current_poly,
+                        [o for o in objects if o.visited],
+                        vision_area,
+                        in_vision_objects)
         all_regions.append(region)
         plot(region, all_points)
         start = end
 
     assert len(all_regions) == len(waypoints)
 
-    return all_regions
+    return all_regions, (min(wall_lengths), max(wall_lengths))
 
 
 def scene_complexity(scene: Scene, filename: str) -> None:
 
-    regions = scene_regions(scene)
+    regions, (w, l) = scene_regions(scene)
 
     with open(filename, 'w') as f:
-        yaml.dump([r.to_dict() for r in regions], f)
+        yaml.dump({
+            'length': l,
+            'width': w,
+            'regions': [r.to_dict() for r in regions]
+            }, f)
+
+
+def load_regions_from_file(filename: str) -> Tuple[List[Region], float, float]:
+
+    with open(filename, 'r') as f:
+        all_regions = yaml.load(f, Loader=yaml.FullLoader)
+
+    return [Region.from_dict(d) for d in all_regions['regions']], all_regions['width'], all_regions['length']
